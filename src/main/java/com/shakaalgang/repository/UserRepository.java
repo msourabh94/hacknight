2448f5c5bdec03d97b003b4031d4cd687c94d386
Sourabh Maharana <msourabh94@live.com>
2020-01-19 02:54:04 +0530

    deploy changes

--- src/main/java/com/shakaalgang/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.shakaalgang.repository;
-
-import com.shakaalgang.entity.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<UserEntity, String> {
-}
